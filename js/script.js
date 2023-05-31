9eb622084460de1ce6423650929a34055a5e0516
taracruickshxnk <taracr2007@gmail.com>
2023-05-31 10:01:41 +0100

    ADD login page, open page, register page

--- js/script.js
+++ js/script.js
@@ -11,3 +11,23 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+
+// LOGIN PAGE
+
+  $(document).ready(function() {
+    // Check if it's the first page
+    if ($('body').hasClass('open')) {
+      $('.topnav').hide(); // Hide the navigation bar
+    }
+  });
+    
+    function redirectTo(pageId) {
+    // Hide all pages
+    var pages = document.getElementsByClassName('page');
+    for (var i = 0; i < pages.length; i++) {
+        pages[i].classList.remove('active');
+    }
+    // Show the selected page
+    var selectedPage = document.getElementById(pageId);
+    selectedPage.classList.add('active');
+  }
